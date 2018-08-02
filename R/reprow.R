bbd79b4ef27545c23ba7533b6e35f7ff1b57da87
James Staley <js16174@bristol.ac.uk>
2018-08-02 13:19:22 +0100

    Delete reprow.R

--- R/reprow.R
+++
@@ -1,9 +0,0 @@
-# Repeat rows
-#
-# This function creates a matrix of a repeated vector where each row is the same
-# @param x vector to be repeated
-# @param n number of repeats
-# @export
-reprow<-function(x,n){
-  matrix(rep(x,each=n),nrow=n)
-}
