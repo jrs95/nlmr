2824020bbd54b14d0157b561f23f98ad912ece53
James Staley <james.staley@bristol.ac.uk>
2019-02-12 13:41:43 +0000

    Update piecewise.R

--- R/piecewise.R
+++ R/piecewise.R
@@ -61,6 +61,7 @@ piecewise_mr <- function(y, x, g, covar=NULL, family="gaussian", q=10, xpos="mea
   
   ##### Covariates #####
   if(!is.null(covar)){covar <- as.matrix(model.matrix(as.formula(paste("~ ", paste(names(covar),collapse=" + "))), data=covar)[,-1])}
+  if(any(is.na(covar))) stop('there are missing values in the covariates')
   
   ##### x0 (IV-Free) #####
   ivf <- iv_free(y=y, x=x, g=g, covar=covar, q=q, family=family)
